3675c18f64aa29be9a3fcc1486f6f80c3a0743eb
立婷 <0225pisces@gmail.com>
2023-03-31 18:16:41 +0800

    20230331 commit

---
+++ vue-project/project0330/src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>    
+  <img class="logo" src="../assets/fubon-logo.png" alt="">
+<h1>
+    Sign Up
+</h1>
+</template>
+
+<script>
+export default{
+    name:'SignUp'
+}
+</script>
+
+<style>
+.logo{
+    width: 100px;
+}
+
+</style>
